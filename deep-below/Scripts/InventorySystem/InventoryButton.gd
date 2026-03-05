28b614849887e46121568c5281115a19aefff8e2
Approxamatrix <pokekid919@protonmail.com>
2026-03-04 22:51:55 -0400

    started working on the item slot moving mechanic

--- deep-below/Scripts/InventorySystem/InventoryButton.gd
+++ deep-below/Scripts/InventorySystem/InventoryButton.gd
@@ -1,8 +1,10 @@
 extends Button
+class_name InventoryButton
 
 @export var Itemicon : TextureRect
 @export var Itemamt : Label
 
+signal pressedwithref(button)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +14,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_pressed() -> void:
+	
+	pressedwithref.emit(self)
+	
+	pass # Replace with function body.
--- deep-below/Scripts/InventorySystem/InventoryGUIManager.gd
+++ deep-below/Scripts/InventorySystem/InventoryGUIManager.gd
@@ -6,6 +6,8 @@ extends Control
 @export var InvUI : GridContainer
 
 
+var selecteditem = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -13,6 +15,7 @@ func _ready() -> void:
 	
 	
 	if InvNode != null:
+		prepare_button_signals()
 		if InvNode.InventoryData != null:
 			prepare_inventory_icons()
 			populate_inventory_ui()
@@ -24,6 +27,21 @@ func _ready() -> void:
 		pass
 	
 	pass # Replace with function body.
+
+func prepare_button_signals():
+	
+	for nodes in InvUI.get_children():
+		
+		if nodes is Button:
+			
+			nodes.connect("pressedwithref",idk)
+			
+			pass
+		
+		pass
+	
+	pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -81,5 +99,19 @@ func populate_inventory_ui():
 		
 		pass
 	#
+	
+	pass
+
+func idk(buttonref : InventoryButton):
+	
+	print("idk")
+	print(buttonref)
+	
+	if selecteditem == null:
+		
+		buttonref = selecteditem
+		
+		pass
+	
 	
 	pass
